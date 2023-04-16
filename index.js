39dc0d97aac0e98ad66659c723ea26ee517b4e5c
ridim02 <99312945+ridim02@users.noreply.github.com>
2023-04-16 06:52:31 +0545

    added reply functionality
    now sends a message only if it is the command '/quote' and hopefully sends a reply as the original message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv/config');
-
+const prefix = '/';
 // Create a new client instance
 // src = https://discordjs.guide/popular-topics/intents.html#enabling-intents
 const client = new Client({
@@ -9,7 +9,6 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
 	],
 });
 
@@ -19,8 +18,16 @@ client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('messageCreate', (message) => {
-    console.log(message);
+client.on("messageCreate", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  else if (message.content.startsWith(`${prefix}quote`)) {
+    //to read original message from a reply
+    const repliedMessage = await message.fetchReference();
+    console.log(repliedMessage.content);
+    message.channel.send(`${repliedMessage.content}`);
+  }
 });
 
 // Log in to Discord with your client's token
